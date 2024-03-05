438b3e03e3b7ad70dfbce6de7051c2fbce9ea87e
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-05 11:15:42 +0100

    ESlint checks

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
-    
-    if (trueOrFalse) {
-        let task = true;
-        let task2 = false;
-    }
-    return [task, task2];
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
+  }
+  return [task, task2];
+// eslint-disable-next-line eol-last
 }
