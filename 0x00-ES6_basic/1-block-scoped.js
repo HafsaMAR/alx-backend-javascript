39c0e3e9e32d7341111e5efcac523281baa331a6
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-05 11:27:49 +0100

    Value of variables cannot be changed error

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 export default function taskBlock(trueOrFalse) {
-  var task = false; // Changed var to let
-  var task2 = true; // Changed var to let
+  const task = false; // Changed var to let
+  const task2 = true; // Changed var to let
 
   if (trueOrFalse) {
-    let task = true; // Removed var keyword
-    let task2 = false; // Removed var keyword
+    const task = true; // Removed var keyword
+    const task2 = false; // Removed var keyword
   }
 
   return [task, task2];
