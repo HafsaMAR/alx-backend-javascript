0dfd33caea7bf03aa7ac272506390e25ee51aa4a
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-06 14:59:00 +0100

    ESlint checks

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,7 +4,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true; // Changed var to let
 
   if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars, no-shadow
     const task = true; // Removed var keyword
+    // eslint-disable-next-line no-shadow, no-unused-vars
     const task2 = false; // Removed var keyword
   }
 
