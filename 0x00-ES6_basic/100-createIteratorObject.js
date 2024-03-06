7751ac002367eacdbc6e03c62bfdffea4693a166
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-06 22:17:26 +0100

    iterator protocol

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+/* eslint-disable no-restricted-syntax */
+export default function createIteratorObject(report) {
+  let currentDepartmentIndex = 0;
+  let currentEmployeeIndex = 0;
+
+  return {
+    next() {
+      if (currentDepartmentIndex < report.length) {
+        const department = report[currentDepartmentIndex];
+        const { employees } = department;
+
+        if (currentEmployeeIndex < employees.length) {
+          const employee = employees[currentEmployeeIndex];
+          // eslint-disable-next-line no-plusplus
+          currentEmployeeIndex++;
+          return { value: employee, done: false };
+        }
+        // eslint-disable-next-line no-plusplus
+        currentDepartmentIndex++;
+        currentEmployeeIndex = 0;
+        return this.next();
+      }
+      return { done: true };
+    },
+  };
+}
