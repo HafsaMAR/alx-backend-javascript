f73af358431bf4eba0c4b5a9cf2720df35f2f37c
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-06 15:16:06 +0100

    ES6 Method Properties

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 // Rewrite getFullBudgetObject to use ES6 method properties in the fullBudget object
 
+// eslint-disable-next-line import/extensions
 import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: function (income) {
-      return `$${income}`;
+    getIncomeInDollars() {
+      return `$${this.income}`;
     },
-    getIncomeInEuros: function (income) {
-      return `${income} euros`;
+    getIncomeInEuros() {
+      return `${this.income} euros`;
     },
   };
 
