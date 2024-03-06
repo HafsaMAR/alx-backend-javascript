a559b2e53d1cd389bc1c8bf6c43b23abcce39ab3
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-06 15:40:59 +0100

    loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  for (let i = 0; i < array.length; i += 1) {
+    // eslint-disable-next-line no-param-reassign
+    array[i] = appendString + array[i];
+  }
+
+  return array;
+}
