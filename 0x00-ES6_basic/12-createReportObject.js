db61cabe9d69c7e3515830953337e0184e42e694
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-06 16:53:18 +0100

    Object Creation, Method Definition, and ES6 Syntax

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+export default function createReportObject(employeesList) {
+  const report = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
+  };
+  return report;
+}
