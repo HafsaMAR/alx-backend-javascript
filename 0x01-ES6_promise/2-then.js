e0fd116fb8133d4a555fd87fef18f04f3ed4fd78
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-07 17:15:28 +0100

    Implement Finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,11 +3,10 @@ export default function handleResponseFromAPI(promise) {
   promise
     .then(() => {
       const response = { status: 200, body: 'Success' };
-      console.log('Got a response from the API');
       return response;
     })
-    .catch(() => {
-      console.error('Got a response from the API');
-      return new Error();
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
     });
 }
