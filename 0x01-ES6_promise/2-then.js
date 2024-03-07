92338e442dadc5658cd3fef62c35914c062dfe09
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-07 17:21:21 +0100

    error handling

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,13 +3,11 @@ export default function handleResponseFromAPI(promise) {
   promise
     .then(() => {
       const response = { status: 200, body: 'Success' };
+      console.log('Got a response from the API');
       return response;
     })
-    // eslint-disable-next-line arrow-body-style
     .catch(() => {
+      console.error('Got a response from the API');
       return new Error();
-    })
-    .finally(() => {
-      console.log('Got a response from the API');
     });
 }
