9919676b777e1a86e7678b9ceaa218a8e9fb8081
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-08 18:04:58 +0100

    promises value, status and reason

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(filename);
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((result) => result.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })))
+    .catch((error) => {
+      console.error('Error:', error);
+      return [];
+    });
+}
