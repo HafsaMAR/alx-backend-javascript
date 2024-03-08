21a54be8b15fb9634a96403167ab3dd8ec4d5584
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-08 15:43:01 +0100

    explicitly reject a promise

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
-    reject(new Error(`${filename} cannot be processed`));
-  });
+  return (Promise.reject(new Error(`${filename} cannot be processed`)));
 }
