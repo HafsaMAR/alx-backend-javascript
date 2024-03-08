be1f477e3c73607dce039e3c2819dd71306b1dcd
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-08 18:19:49 +0100

    reason to string

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,15 +4,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, filename) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(filename);
-  return Promise.allSettled([userPromise, photoPromise])
-    .then((result) => result.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })))
-    .catch((error) => {
-      console.error('Error:', error);
-      return [];
-    });
+  const allPromises = await Promise.allSettled([signUpUser(firstName, lastName),
+    uploadPhoto(filename)]);
+  return allPromises.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
+  }));
 }
