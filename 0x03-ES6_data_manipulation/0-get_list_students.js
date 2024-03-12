d57c5c78f5f27913835d185f68b61f3bb06a8778
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-12 19:38:53 +0000

    quote string

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,9 +2,9 @@
 
 export default function getListStudents() {
   const arrayOfObjects = [
-    { id: 1, firstname: "Guillaume", location: "San Francisco" },
-    { id: 2, firstname: "James", location: "Columbia" },
-    { id: 3, firstname: "Serena", location: "San Francisco" },
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 3, firstname: 'Serena', location: 'San Francisco' },
 
   ];
   return arrayOfObjects;
