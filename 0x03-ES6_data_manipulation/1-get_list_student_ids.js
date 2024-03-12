6c3e1a073cf8dbecf60cd5f6555c28bfa54c8f58
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-12 22:35:31 +0000

    array of attributes

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function getListStudentIds(students) {
+    // Check if the input is an array
+    if (!Array.isArray(students)) {
+      return [];
+    }
+    
+    // Use the map function to extract ids from each student object
+    return students.map(student => student.id);
+}
