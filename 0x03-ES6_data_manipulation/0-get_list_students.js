b2d26c3e0a9f4f74c0c77f764e09bce0f5c5516c
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-12 19:51:53 +0000

    small changes

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,7 @@
-#!/usr/bin/node
-
 export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 3, firstName: 'Serena', location: 'San Francisco' },
-
+    { id: 3, firstName: 'Serena', location: 'San Francisco' }
   ];
 }
