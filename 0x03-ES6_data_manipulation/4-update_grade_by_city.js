f09a7f7ddb1fd91a5e3b1428049eec6c68ac05cb
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-13 08:14:03 +0000

    map filter and find combined

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+export default function updateStudentGradeByCity(Students, city, newGrades) {
+  // filter the Students array using filter
+  const filteredStudents = Students.filter((Student) => Student.location === city);
+  // map the filter array to add the attribute grade to the students array
+  const updateStudents = filteredStudents.map((Student) => {
+    // check if the student has a new grade to be updated
+    const gradeInfo = newGrades.find((grade) => grade.studentId === Student.id);
+    // implement a the new attribute to the array of students objects
+    const Grade = gradeInfo ? gradeInfo.grade : 'N/A';
+
+    return {
+      ...Student,
+      grade: Grade,
+    };
+  });
+
+  return updateStudents;
+}
