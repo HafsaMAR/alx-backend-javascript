05ca88417950932723f2cfe4f68d4312b0951680
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-13 10:58:25 +0000

    arrayBuffer and DataView set

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+export default function createInt8TypedArray(length, position, value) {
+  const Buffer = new ArrayBuffer(length);
+
+  // check the postion
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const view = new DataView(Buffer);
+  view.setInt8(position, value);
+
+  return view;
+}
