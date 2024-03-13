9272a7791e8a0e0f512a82a16a6047771bbb5c2b
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-13 07:14:03 +0000

    test check errors

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,9 @@
 export default function getListStudentIds(students) {
-    // Check if the input is an array
-    if (!Array.isArray(students)) {
-      return [];
-    }
-    
-    // Use the map function to extract ids from each student object
-    return students.map(student => student.id);
+  // Check if the input is an array
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  // Use the map function to extract ids from each student object
+  return students.map((student) => student.id);
 }
