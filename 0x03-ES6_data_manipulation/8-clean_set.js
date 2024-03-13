3afb9cdd33980099a6b215db101472a3c212af23
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-13 13:22:37 +0000

    use of start, push and join methods

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function cleanSet(Set, startString) {
+  const cleanedValue = [];
+
+  Set.forEach((element) => {
+    if (element.startsWith(startString)) {
+      cleanedValue.push(element.slice(startString.length));
+    }
+  });
+
+  return cleanedValue.join('-');
+}
