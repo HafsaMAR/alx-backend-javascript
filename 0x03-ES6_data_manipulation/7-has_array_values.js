6503d87373c2fe3b877c19e7f4de6f364b74ad1a
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-13 12:51:53 +0000

    set and data stucture

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function hasValuesFromArray(set, array) {
+  let result = true;
+  array.forEach((element) => {
+    if (set.has(element) === false) {
+      result = false;
+    }
+  });
+  return result;
+}
