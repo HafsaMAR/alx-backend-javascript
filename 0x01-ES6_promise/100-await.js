49b19bbf42dd7635c5fbc94425df76e61e1e7b16
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-17 12:25:04 +0000

    AWAIT

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  const [photo, user] = await Promise.all([
+    uploadPhoto(),
+    createUser(),
+  ]);
+
+  return !photo || !user ? { photo: null, user: null } : { photo, user };
+}
