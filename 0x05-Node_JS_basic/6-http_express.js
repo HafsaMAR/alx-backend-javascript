ebd6fc6a7c60039d2c6d1d94acafa8366ee4ce29
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-30 22:10:08 +0100

    I don't know what I'm doing any more

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const express = require('express');
+const app = express();
+
+// Define a route handler for the root endpoint
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+});
+
+// Set up the server to listen on port 1245
+const PORT = 1245;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// Export the app instance
+module.exports = app;
