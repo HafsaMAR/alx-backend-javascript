53beb4c63e02fdbebd34a3ccd2180f808d5173bb
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-30 21:33:08 +0100

    sync file read and parsing

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,34 +3,44 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  let data;
   try {
-    data = fs.readFileSync(path, 'utf8');
+    const results = fs.readFileSync(path, { encoding: 'utf8' }).split(/\r?\n/);
+    const lines = results;
+    let i = 0;
+    let countStudents = 0;
+    const fields = {};
+
+    for (const line of lines) {
+      if (line.trim() !== '' && i > 0) {
+        countStudents += 1;
+          const [fname, lname, age, field] = line.split(','); // eslint-disable-line
+        if (!fields[field]) {
+          fields[field] = {
+            count: 1,
+            students: [fname],
+          };
+        } else {
+          const newCount = fields[field].count + 1;
+          const newStudents = (fields[field].students).concat(fname);
+          fields[field] = {
+            count: newCount,
+            students: newStudents,
+          };
+        }
+      }
+      i += 1;
+    }
+    // eslint-disable-next-line no-console
+    console.log(`Number of students: ${countStudents}`);
+    for (const field of Object.keys(fields)) {
+      const n = fields[field].count;
+      const names = fields[field].students.join(', ');
+      // eslint-disable-next-line no-console
+      console.log(`Number of students in ${field}: ${n}. List: ${names}`);
+    }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
-  // parse the csv data
-  const rows = data.trim().split('\n');
-  const students = rows.slice(1).map((line) => line.split(','));
-  const fields = {};
-
-  students.forEach((student) => {
-    const field = student[3];
-    // start the list of students in each field
-    if (!fields[field]) {
-      fields[field] = [];
-    }
-
-    fields[field].push(student[0]);
-
-    // log the list of students of each list
-  });
-  // eslint-disable-next-line no-console
-  console.log('Number of students:', students.length);
-  Object.entries(fields).forEach(([field, fieldStudents]) => {
-    // eslint-disable-next-line no-console
-    console.log(`Number of students in ${field}: ${fieldStudents.length}. List: ${fieldStudents.join(', ')}`);
-  });
 }
 
 module.exports = countStudents;
