32514c8bade3c091b43fb11b4de402e5ad4ab56b
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-30 21:49:50 +0100

    http request

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 1245;
+
+// create the HTTP server
+const app = http.createServer((req, res) => {
+  // set the response headers
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+  // console.log(`Server running at ${hostname}:${port}`);
+});
+
+module.exports = app;
