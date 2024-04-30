d7243592816cb6389c5737a3bf78f0535dc71b92
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-30 21:29:18 +0100

    async file read and parsing

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,1 +1,41 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line no-undef
+    data = fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        // parse the csv data
+        const rows = data.trim().split('\n');
+        const students = rows.slice(1).map((line) => line.split(','));
+        const fields = {};
+
+        students.forEach((student) => {
+          const field = student[3];
+          // start the list of students in each field
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+
+          fields[field].push(student[0]);
+
+          // log the list of students of each list
+        });
+        // eslint-disable-next-line no-console
+        console.log('Number of students:', students.length);
+        Object.entries(fields).forEach(([field, fieldStudents]) => {
+          // eslint-disable-next-line no-console
+          console.log(`Number of students in ${field}: ${fieldStudents.length}. List: ${fieldStudents.join(', ')}`);
+        });
+
+        resolve();
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
