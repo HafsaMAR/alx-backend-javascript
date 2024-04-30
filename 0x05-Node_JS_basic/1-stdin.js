0e07ac8fedd0e90a9dcb300b0ce672a39f2602a9
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-30 19:30:01 +0100

    eslint fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,19 @@
 #!/usr/bin/node
 
 // read input from standard input
-process.stdin.setEncoding('utf-8')
+process.stdin.setEncoding('utf-8');
 
 // Listen to the user input after asking question
+// eslint-disable-next-line no-console
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', function(data) {
-    const name = data.trim(); // trim whitespaces from input
-    console.log(`Your name is: ${name}`);
+process.stdin.on('data', (data) => {
+  const name = data.trim(); // trim whitespaces from input
+  // eslint-disable-next-line no-console
+  console.log(`Your name is: ${name}`);
 
-    // Handle termination of the program
-    process.stdin.on('end', function() {
-        console.log('This important software is now closing')
-    })
-})
+  // Handle termination of the program
+  process.stdin.on('end', () => {
+    // eslint-disable-next-line no-console
+    console.log('This important software is now closing');
+  });
+});
