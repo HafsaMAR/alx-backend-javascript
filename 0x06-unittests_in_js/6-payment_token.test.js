3e52778b5c7d749a6ed574248e08f2a7dca1e7d4
HafsaMAR <marouche.hafsa@gmail.com>
2024-05-02 23:54:55 +0100

    Async tests

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+const assert = require('assert');
+const getPaymentTokenFromAPI = require('./6-payment_token.js');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a promise', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        assert.equal(res.data, 'Successful response from the API');
+        done();
+      })
+      .catch((error) => done(error));
+  });
+});
