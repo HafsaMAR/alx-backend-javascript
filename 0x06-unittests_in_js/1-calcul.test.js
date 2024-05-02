fcf9351ab9ef2fcc540bd1b00000af36a1aa37c3
HafsaMAR <marouche.hafsa@gmail.com>
2024-05-02 23:50:17 +0100

    calculate Number

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,1 +1,20 @@
-#!/usr/bin/node
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  it('should add two numbers', () => {
+    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+  });
+
+  it('should subtract two numbers', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  });
+
+  it('should divide two numbers', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
+
+  it('should return Error when dividing by zero', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  });
+});
